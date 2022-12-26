13f50a2db4138cef5629a8016bcae0c20c7e93f8
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2022-12-26 13:39:11 -0500

    Add the move of enemy with AI

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
