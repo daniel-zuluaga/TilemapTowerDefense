cdaf6adc5ffa87fac00509afacdfafc5e531b308
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2022-12-30 16:53:11 -0500

    finsih to organize the shoot of the machine gun where is torret you can shoot him now to the enemys

---
+++ Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Transform target;
+    public GameObject ImpactEffects;
+
+    public float speed = 70f;
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+
+    void Update()
+    {
+        VerificationBulletTarget();
+    }
+
+    void VerificationBulletTarget()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+    }
+
+    void HitTarget()
+    {
+        GameObject effectsObj = Instantiate(ImpactEffects, transform.position, transform.rotation);
+        Destroy(effectsObj, 2f);
+
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player/Torret.cs
+++ Assets/Scripts/Player/Torret.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 public class Torret : MonoBehaviour
 {
     public Transform target;
-    public float range = 15f;
 
+    [Header("Attributes")]
+    public float range = 15f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    [Header("Unity Setup Fields")]  
     public string enemyTag = "Enemy";
+
+    public Transform partToRotate;
+    public float turnSpeed;
+
+    [Header("Shooting")]
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public GameObject effectShoot;
 
     // Start is called before the first frame update
     void Start()
@@ -18,17 +31,66 @@ public class Torret : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        if (target == null)
+            return;
 
-        foreach (GameObject enemy in enemies)
+        // Target look on
+        RotateTurret();
+    }
+
+    void RotateTurret()
+    {
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if(fireCountdown <= 0f)
         {
-            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        GameObject effectShootObj = Instantiate(effectShoot, firePoint.position, firePoint.rotation);
+        GameObject bulletGO = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+        Destroy(effectShootObj, 1f);
+
+        if (bullet != null)
+        {
+            bullet.Seek(target);
         }
     }
 
     void UpdateTarget()
     {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnenmy = null;
 
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnenmy = enemy;
+            }
+        }
+
+        if (nearestEnenmy != null && shortestDistance <= range)
+        {
+            target = nearestEnenmy.transform;
+        }
+        else
+        {
+            target = null;
+        }
     }
 
     private void OnDrawGizmosSelected()
