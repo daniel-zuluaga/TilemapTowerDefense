3197c412ff480687ab4c26c997afaaad7b78993d
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2023-01-11 19:28:13 -0500

    Finish of add the turret from laser and the enemy will have a slowdown when the laser hits it

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -2,17 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Enemy))]
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    public Enemy enemy;
+
     void Start()
     {
-        
+        target = Waypoints.points[0];
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * enemy.speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            enemy.rotEnemy.transform.Rotate(enemy.rotateEnemydir.y * Vector3.up, Space.World);
+            GetNextPoints();
+        }
+
+        enemy.speed = enemy.startSpeed;
+    }
+
+    void GetNextPoints()
+    {
+        if (wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            EndPath();
+            return;
+        }
+
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+
+    void EndPath()
+    {
+        Destroy(gameObject);
+        PlayerStats.lives -= enemy.damagePlayer;
     }
 }
